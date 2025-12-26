2a45402d2fce80c1609501beb70cd24324e844b6
Varsha <varshanelakurthi@gmail.com>
2025-12-26 12:25:36 -0600

    Fix CI test failures by adding login

--- src/test/java/com/yourname/automation/tests/LoginTest.java
+++ src/test/java/com/yourname/automation/tests/LoginTest.java
@@ -1,11 +1,22 @@
 package com.yourname.automation.tests;
-import com.microsoft.playwright.*;
-import com.yourname.automation.pages.LoginPage;
+
 import com.yourname.automation.base.baseTest;
+import com.yourname.automation.pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends baseTest {
+
+    @Test
+    public void validLoginTest() {
+        LoginPage lp = new LoginPage(page);
+
+        lp.navigate();
+        lp.login("standard_user", "secret_sauce");
+
+        page.waitForURL("**/inventory.html");
+        Assert.assertTrue(page.title().contains("Swag Labs"));
+    }
 
     @Test
     public void inventoryPageTest() {
@@ -15,6 +26,6 @@ public class LoginTest extends baseTest {
         lp.login("standard_user", "secret_sauce");
 
         page.waitForURL("**/inventory.html");
-        Assert.assertTrue(page.title().contains("Swag Labs"));
+        Assert.assertTrue(page.url().contains("inventory"));
     }
 }
