0edf00a00eb7e8c903e75c58359d868e260e473b
Varsha <varshanelakurthi@gmail.com>
2025-12-26 13:36:43 -0600

    Fix BaseTest naming and LoginTest impor

--- src/test/java/com/yourname/automation/tests/LoginTest.java
+++ src/test/java/com/yourname/automation/tests/LoginTest.java
@@ -1,27 +1,16 @@
 package com.yourname.automation.tests;
 
-import com.yourname.automation.base.baseTest;
+import com.yourname.automation.base.BaseTest;
 import com.yourname.automation.pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginTest extends baseTest {
+public class LoginTest extends BaseTest {
 
     @Test
     public void validLoginTest() {
+
         LoginPage lp = new LoginPage(page);
-
-        lp.navigate();
-        lp.login("standard_user", "secret_sauce");
-
-        page.waitForURL("**/inventory.html");
-        Assert.assertTrue(page.title().contains("Swag Labs"));
-    }
-
-    @Test
-    public void inventoryPageTest() {
-        LoginPage lp = new LoginPage(page);
-
         lp.navigate();
         lp.login("standard_user", "secret_sauce");
 
